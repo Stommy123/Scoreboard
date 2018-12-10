681002df6681dfbfcea6241e092fe5ae0d6085df
Tommy <tsuwunrut@carecloud.com>
2018-12-10 15:15:48 -0500

    more indents

--- src/App.js
+++ src/App.js
@@ -6,53 +6,53 @@ import playersList from './data/playerData';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      players: playersList,
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			players: playersList,
+		}
+	}
 
-  addPlayers = (name, age) => {
-    const { players } = this.state 
-    players.push({
-      name: name,
-      id: players.length + 1,
-      score: 0,
-      age: age
-    })
-    this.setState({ players })
-  }
+	addPlayers = (name, age) => {
+		const { players } = this.state 
+		players.push({
+			name: name,
+			id: players.length + 1,
+			score: 0,
+			age: age
+		})
+		this.setState({ players })
+	}
 
-  removePlayer = id => {
-    let { players } = this.state 
-    players = players.filter( p => p.id !== id )
-    this.setState({ players })
-  }
+	removePlayer = id => {
+		let { players } = this.state 
+		players = players.filter( p => p.id !== id )
+		this.setState({ players })
+	}
 
-  handleScoreChange = (i, e) => {
-    let { players } = this.state
-      players[i].score += e
-      this.setState({ players })
-  }
+	handleScoreChange = (i, e) => {
+		let { players } = this.state
+			players[i].score += e
+			this.setState({ players })
+	}
 
-  getHighScore = () => {
-    const scores = this.state.players.map(s => s.score)
-    const highScore = Math.max(...scores)
-    if (highScore) return highScore
-  }
+	getHighScore = () => {
+		const scores = this.state.players.map(s => s.score)
+		const highScore = Math.max(...scores)
+		if (highScore) return highScore
+	}
 
-  render() {
-    const { players } = this.state
-    const highScore = this.getHighScore()
-    return (
-      <div className="scoreboard">
-        <Header title="Scoreboard" players={players} />
-        <Players highScore={highScore} changeScore={this.handleScoreChange} removePlayer={this.removePlayer} players={players} />
-        <Form addPlayer={this.addPlayers} />
-      </div>
-    );
-  }
+	render() {
+		const { players } = this.state
+		const highScore = this.getHighScore()
+		return (
+			<div className="scoreboard">
+				<Header title="Scoreboard" players={players} />
+				<Players highScore={highScore} changeScore={this.handleScoreChange} removePlayer={this.removePlayer} players={players} />
+				<Form addPlayer={this.addPlayers} />
+			</div>
+		);
+	}
 }
 
 export default App;
